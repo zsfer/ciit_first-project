50ed1aaa15ea3ac7000949bca096154b47ceb41f
zsfer <seiferalbacete@gmail.com>
2023-05-30 13:58:51 +0800

    feat(event): added lvl 1 events

--- Assets/Scripts/TextGameManager.cs
+++ Assets/Scripts/TextGameManager.cs
@@ -6,11 +6,11 @@ using TMPro;
 public class TextGameManager : MonoBehaviour
 {
     [Header("Player")]
-    public float Health = 100f;
-    public float Stamina = 100f;
+    public int Health = 10;
+    public int Stamina = 10;
 
     [Header("Game")]
-    [SerializeField] [TextArea(3, 50)] private string m_storyText = string.Empty;
+    [SerializeField] [TextArea(3, 50)] private string m_gameText = string.Empty;
     
     [Header("UI")]
     [SerializeField] private TextMeshProUGUI m_gameTextUI;
@@ -19,14 +19,54 @@ public class TextGameManager : MonoBehaviour
 
     void Start()
     {
+
+    }
+
+    private void Update()
+    {
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
         m_healthTextUI.text = Health.ToString();
         m_staminaTextUI.text = Stamina.ToString();
 
-        m_gameTextUI.text = m_storyText;
+        m_gameTextUI.text = m_gameText;
     }
 
-    void Update()
+
+    #region Game Events
+
+    public void Lvl1_OpenDoor()
     {
-        
+        if (Stamina <= 0)
+        {
+            m_gameText = "You're tired";
+            return;
+        }
+        m_gameText = "The door won't budge.";
+        Stamina -= 1;
     }
+
+    public void Lvl1_OpenWindow()
+    {
+        if (Stamina <= 0)
+        {
+            m_gameText = "You're too tired to do this";
+            return;
+        }
+
+        m_gameText = "You try to push the window open but it is sealed shut. \nYou fall down trying to push it open. [-1HP, -5STA]";
+        Health -= 1;
+        Stamina -= 5;
+    }
+
+    public void Lvl1_Wait()
+    {
+        m_gameText = "You wait. [10STA]";
+        Stamina = 10;
+    }
+
+    #endregion
 }
